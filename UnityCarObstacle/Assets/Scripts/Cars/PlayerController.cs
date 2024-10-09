da06e811fe8d4635841d0a2caf8e910408629949
Farnood <sotoudehfarnood@gmail.com>
2024-10-09 23:00:55 +0330

    Add files via upload

---
+++ UnityCarObstacle/Assets/Scripts/Cars/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float verticalInput;
+    public Rigidbody vehicleRigidBody;
+    private bool hasWon = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput * verticalInput);
+        if (transform.position.z > 164 && !hasWon)
+        {
+            vehicleRigidBody.AddForce(Vector3.up * vehicleRigidBody.mass * 1000);
+            hasWon = true;
+        }
+    }
+}
